57837709396d542dbadfea85be09c259c95aa8ef
wind <tzy2005@qq.com>
2019-03-28 10:11:50 +0800

    wind committed

--- 06/printTable.py
+++ 06/printTable.py
@@ -11,9 +11,9 @@ for i in range(len(tableData)):
     for k in range(len(tableData[i])-1):
         if len(tableData[i][k]) <= len(tableData[i][k+1]):
             max = len(tableData[i][k+1])
-        cloWiths.append(max)
+    cloWiths.append(max)
 
 for k in range(len(tableData[0])):
     for i in range(len(tableData)):
-        print(tableData[i][k].rjust(cloWiths[i]), end='')
+        print(tableData[i][k].rjust(cloWiths[i]), end=' ')
     print()
